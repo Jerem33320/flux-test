67fea1eee1c997ab1528f727bd42872fa1d379c0
Jeremy Peyrat <prstoad@gmail.com>
2020-11-02 15:02:37 +0100

    working on flux with react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import AppActions from '../src/js/actions/app-actions';
+import Catalog from '../src/js/components/app-catalog';
+import Cart from './js/components/app-cart';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component{
+  render(){
+    return(
+      <div className="container">
+        <Catalog />
+        <Cart />
+      </div>
+    )
+  }
 }
-
-export default App;
